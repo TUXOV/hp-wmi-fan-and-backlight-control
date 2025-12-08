2b1347edb2c8924b134ce06704a229e5022bc94f
Edip Hazuri <edip@medip.dev>
2025-12-08 14:25:29 +0300

    backport: add 8c78 for victus_s thermal profile support
    this backports: https://github.com/torvalds/linux/commit/fa0498f8047536b877819ce4ab154d332b243d43

--- hp-wmi.c
+++ hp-wmi.c
@@ -100,7 +100,9 @@ static const char * const victus_thermal_profile_boards[] = {
 
 /* DMI Board names of Victus 16-r and Victus 16-s laptops */
 static const char * const victus_s_thermal_profile_boards[] = {
-	"8BBE", "8BD4", "8BD5", "8C99", "8C9C", "8D41"
+	"8BBE", "8BD4", "8BD5",
+	"8C78", "8C99", "8C9C",
+	"8D41",
 };
 
 enum hp_wmi_radio {
